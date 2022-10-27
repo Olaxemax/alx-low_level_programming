d96db1ff40f2c30316e1a006b237615112fa5284
Olaxemax <113837012+Olaxemax@users.noreply.github.com>
2022-10-27 13:33:07 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies at most an inputted number
- *            of bytes from string src into dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
+ * _strncpy - concatinate the values of two integers.
  *
- * Return: A pointer to the resulting string dest.
+ * @dest: arg a
+ *
+ * @src: arg b
+ *
+ * @n: arg n
+ *
+ * Return: void.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int i;
 
-	while (src[index++])
-		src_len++;
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
+	dest[i] = '\0';
 
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
 
 	return (dest);
 }
