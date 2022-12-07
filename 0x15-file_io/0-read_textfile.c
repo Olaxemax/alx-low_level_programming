7320bc5180b1802cabe97541bc26a288f235c103
Olaxemax <113837012+Olaxemax@users.noreply.github.com>
2022-12-07 23:31:25 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,71 +1,77 @@
 #include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
+
+
+
+vi 1-create_file.c
+#include "main.h"
 
 /**
-
- * read_textfile - reads a text file and prints the letters
-
+ * create_file - creates a file
  * @filename: filename.
-
- * @letters: numbers of letters printed.
-
+ * @text_content: content writed in the file.
  *
-
- * Return: numbers of letters printed. It fails, returns 0.
-
+ * Return: 1 if it success. -1 if it fails.
  */
-
-ssize_t read_textfile(const char *filename, size_t letters)
-  
+int create_file(const char *filename, char *text_content)
 {
-  
-  int fd;
-  
-  ssize_t nrd, nwr;
-  
-  char *buf;
-  
+	int fd;
+	int nletters;
+	int rwr;
 
-  
-  if (!filename)
-    
-    return (0);
-  
+	if (!filename)
+		return (-1);
 
-  
-  fd = open(filename, O_RDONLY);
-  
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-  
-  if (fd == -1)
-    
-    return (0);
-  
+	if (fd == -1)
+		return (-1);
 
-  
-  buf = malloc(sizeof(char) * (letters));
-  
-  if (!buf)
-    
-    return (0);
-  
+	if (!text_content)
+		text_content = "";
 
-  
-  nrd = read(fd, buf, letters);
-  
-  nwr = write(STDOUT_FILENO, buf, nrd);
-  
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+rwr = write(fd, text_content, nletters);
 
-  
-  close(fd);
-  
+	if (rwr == -1)
+		return (-1);
 
-  
-  free(buf);
-  
+	close(fd);
 
-  
-  return (nwr);
-  
+	return (1);
 }
