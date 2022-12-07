23555dad08bb4b2dab6f03c0482ef3ae1bb5b583
Olaxemax <113837012+Olaxemax@users.noreply.github.com>
2022-12-08 00:02:44 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,41 @@
 #include "main.h"
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * create_file - create a file.
+ * @filename: name of the file to create.
+ * @text_content: text to write inside the file.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 if the file was created, -1 otherwise.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd_open, fd_write;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
+	fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	if (fd_open == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++);
-  	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
-
+	if (text_content != NULL)
+	{
+		fd_write = write(fd_open, text_content, _strlen(text_content));
+		if (fd_write == -1)
+			return (-1);
+	}
+	close(fd_open);
 	return (1);
+}
+
+/**
+ * _strlen - calculate the lenght of a string.
+ * @str: array of characters.
+ *
+ * Return: lenght of the string.
+ */
+int _strlen(char *str)
+{
+	int count = 0;
+
+	while (*str++)
+		count++;
+	return (count);
 }
