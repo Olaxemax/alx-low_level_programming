31ad386b65f823e89e649f1d07b12be4feb38908
Olaxemax <jibaro4u2004@gmail.com>
2022-12-13 04:04:52 -0800

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,8 +1,5 @@
 #include "main.h"
-
 #include <stdlib.h>
-
-
 
 /**
 
@@ -27,7 +24,6 @@ char *create_array(unsigned int size, char c)
 
   
   unsigned int i = 0;
-  
 
   
   s = malloc(size * sizeof(char));
